de9a455c0109eaf6493d019966ac61205b00daba
romulo-xavier25 <romulo.xavier25@gmail.com>
2018-10-30 10:01:17 -0300

    registrando historico de deposito no banco de dados

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -9,9 +9,19 @@ class Balance extends Model
     public $timestamps = false;
 
     public function deposit($valor) : array {
+        $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($valor, 2, '.', '');
         $deposit = $this->save();
-        if($deposit)
+
+        $historic = auth()->user()->historics()->create([
+            'type'          => 'I',
+            'amount'        => $valor,
+            'total_before'  => $totalBefore,
+            'total_after'   => $this->amount,
+            'date'          => date('Ymd'),
+        ]);
+
+        if($deposit && $historic)
             return [
                 'success' => true,
                 'message' => 'deposito realizado com sucesso!'
--- app/Models/Historic.php
+++ app/Models/Historic.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Historic extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['type', 'amount', 'total_before', 'total_after', 'user_id_transaction', 'date',];
+    
 }
